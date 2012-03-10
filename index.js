1cefc3abcf3e257f08fb0cd8bf1da1e7e041b7da
Ben <ben@benrhughes.com>
2012-03-10 23:18:32 +1100

    Fixing addrow button

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ function addRow(val){
 				<input type="text" class="item" value="' + (val || "") + '" /> \
 			</td> \
 			<td class="add"> \
-				<div class="button"><a href="#">+</a></div> \
+				<div class="button"><a id="addButton" href="#">+</a></div> \
 			</td> \
 		</tr>';
 
