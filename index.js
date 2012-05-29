4a4c4d2a2d196cf53b38e3e2387483f3ae7b282e
Ben <ben@benrhughes.com>
2012-05-30 00:21:11 +1000

    Dates in js are weird...

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ function addEntries(){
 		startDate.setDate(startDate.getDate() + offset);
 
 		var endDate = new Date();
-		endDate.setDate(startDate.getDate() + sessionLength);
+		endDate.setDate(endDate.getDate() + offset + sessionLength);
 
 		var params = {
 			"calendarId" : depriveCal.id,
